c4065cb8f59978c36a1572652af476046a7ebbc9
Anthony V <antvanegas@outlook.com>
2023-10-20 21:04:38 -0400

    Created Login Module with input validation

---
+++ cryptosight/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  login(form: NgForm) {
+      
+  }
+}
