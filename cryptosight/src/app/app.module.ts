2952154a637ea62f39f40bc3e5cd94e54983a76f
Anthony V <antvanegas@outlook.com>
2023-10-20 23:04:34 -0400

    Simplified design using Angular Materials.

--- cryptosight/src/app/app.module.ts
+++ cryptosight/src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,        
+    MatInputModule,       
+    MatIconModule,        
+    MatButtonModule,
+    MatToolbarModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
